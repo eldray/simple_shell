44203599446309dd60e944126dab0b7b1e5c5ee1
eldray <emk.appiah@gmail.com>
2022-11-18 15:31:07 -0800

    all pushed

--- get_line.c
+++ get_line.c
@@ -18,7 +18,7 @@ void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
 		*lineptr = buffer;
 	}
 
-	else if (*n < j)
+		else if (*n < j)
 		{
 			if (j > BUFSIZE)
 				*n = j;
@@ -57,34 +57,36 @@ ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
 	input = 0;
 
 	buffer = malloc(sizeof(char) * BUFSIZE);
-								if (buffer == 0)
-return (-1);
-								while (t != '\n')
-								{
-									i = read(STDIN_FILENO, &t, 1);
 
-									if (i == -1 || (i == 0 && input == 0))
-									{
-										free(buffer);
-										return (-1);
-									}
+	if (buffer == 0)
+	return (-1);
+			while (t != '\n')
+			{
+				i = read(STDIN_FILENO, &t, 1);
+
+				if (i == -1 || (i == 0 && input == 0))
+				{
+					free(buffer);
+					return (-1);
+				}
 		if (i == 0 && input != 0)
 		{
 			input++;
 			break;
 		}
 
-		if (input >= BUFSIZE)
+	if (input >= BUFSIZE)
 
-			buffer = _realloc(buffer, input, input + 1);
+		buffer = _realloc(buffer, input, input + 1);
 		buffer[input] = t;
 
 		input++;
-								}
-								buffer[input] = '\0';
-								bring_line(lineptr, n, buffer, input);
-								retval = input;
-								if (i != 0)
-	input = 0;
-return (retval);
+			}
+		buffer[input] = '\0';
+		bring_line(lineptr, n, buffer, input);
+		retval = input;
+
+		if (i != 0)
+			input = 0;
+	return (retval);
 }
